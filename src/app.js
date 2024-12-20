b391aeaead521c6d43e84f50919d24cfdf9055e6
Joao Maria Araujo do Nascimento <1724026@ifrn.local>
2024-12-20 15:13:40 -0300

    Adição de cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 //import {StatusCodes} from "http-status-code";
 
 //Criar o objeto express
@@ -6,7 +7,9 @@ const app = express();
 
 //Configurando o express. No caso, estou pedindo para ele utilizar 
 //o conversor Json do próprio express
+app.use(cors());
 app.use(express.json()); 
+
 
 app.get("/",(req,res) => {
     res.status(200).send("Versão 1.0.0");
